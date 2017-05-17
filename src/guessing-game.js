5934394a50c8605004f559e982af2dbe788a3e59
“LenkouAndrei” <lenkov.andrei.89@gmail.com>
2017-05-17 21:19:14 +0300

    guessing-game/feature: implement binary-search

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.max = null;
+    	this.min = null;
+    	this.mid = null;
+    }
 
     setRange(min, max) {
-
+    	this.max = max;
+    	this.min = min;
     }
 
     guess() {
-
+    	this.mid = Math.ceil(this.min + ((this.max - this.min)/2));
+    	return this.mid;
     }
 
     lower() {
-
+    	this.max = (this.mid);
     }
 
     greater() {
-
+    	this.min = (this.mid);
     }
 }
 
